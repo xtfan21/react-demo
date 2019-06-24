42288c2d45c23e88a9fa6a3d38da5550285856dc
xiaotong.fan <xiaotong.fan@shuyun.com>
2019-06-24 15:38:26 +0800

    message提示信息组件提交代码合并
    去掉多余代码

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Message from '../components/Message/index';
 
 export default class Home extends React.Component {
+
+    handleClick() {
+        Message.success('更改成功');
+        Message.error('更新失败');
+    }
+
     render() {
         return (
             <div className="app-contain">
                 this is 主页内容
+                <button onClick={this.handleClick}>提交</button>
             </div>
         )
     }
